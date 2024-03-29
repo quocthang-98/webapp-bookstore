e201fc07666a7ca9de153089964e70feafe62c65
dodinhphuc <ddp.phucdo@gmail.com>
2024-03-29 09:59:52 +0700

    update DAO

--- src/main/java/com/webapp/dao/impl/BookDAO.java
+++ src/main/java/com/webapp/dao/impl/BookDAO.java
@@ -1,18 +1,12 @@
 package com.webapp.dao.impl;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.List;
 
 import com.webapp.dao.IBookDAO;
 import com.webapp.mapper.BookMapper;
 import com.webapp.model.BookModel;
 
-public class BookDAO extends AbstractDAO<BookModel> implements IBookDAO{
-
+public class BookDAO extends AbstractDAO<BookModel> implements IBookDAO {
 
 	@Override
 	public List<BookModel> findByType(Long id) {
@@ -25,5 +19,5 @@ public class BookDAO extends AbstractDAO<BookModel> implements IBookDAO{
 		String sql = "INSERT INTO book (title, price, type_id) VALUE(?, ?, ?)";
 		return insert(sql, bookModel.getTitle(), bookModel.getPrice(), bookModel.getTypeId());
 	}
-		
+
 }
